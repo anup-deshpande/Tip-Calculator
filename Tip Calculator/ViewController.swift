4239ab0cd88aa07f0868a949a85b6f7ec0133dc5
Deshpande <adeshp16@cci0bhgq18aws.dyn.uncc.edu>
2019-05-22 17:35:23 -0400

    Initial Commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -8,13 +8,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate{
 
+    @IBOutlet weak var billValueTextField: UITextField!
+    @IBOutlet weak var tipChoiceSelector: UISegmentedControl!
+    @IBOutlet weak var customTipSlider: UISlider!
+    @IBOutlet weak var calculatedTipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var customTipPercentageLabel: UILabel!
+    
+    @IBOutlet weak var ImageBackground: UIImageView!
+    
+    var tipPercentage = 10.0
+    var billAmount = 0.0
+    var tip = 0.00
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        billValueTextField.delegate = self
+        customTipSlider.isEnabled = false
+
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func clearAllClicked(_ sender: UIButton) {
+        billValueTextField.text = ""
+        tipChoiceSelector.selectedSegmentIndex = 0
+        customTipPercentageLabel.text = "25 %"
+        totalLabel.text = "0.00"
+        calculatedTipLabel.text = "0.00"
+        customTipSlider.isEnabled = false
+    }
+    
+    @IBAction func textEdited(_ sender: UITextField) {
+      print("Text Edited")
+      billAmount = Double(sender.text!) ?? 0.00
+      calculateTotal()
+    }
+    
+    
+    @IBAction func segmentChoiceChanged(_ sender: UISegmentedControl) {
+        
+        if billValueTextField.text != ""{
+            billAmount = Double(billValueTextField.text!) ?? 0.00
+        }
+        
+        if sender.selectedSegmentIndex == 3 {
+            customTipSlider.isEnabled = true
+            tipPercentage = Double(customTipSlider.value.rounded())
+            calculateTotal()
+            
+        }else{
+            customTipSlider.isEnabled = false
+            
+            if sender.selectedSegmentIndex == 0{
+                tipPercentage = 10
+            }else if sender.selectedSegmentIndex == 1{
+                tipPercentage = 15
+            }else if sender.selectedSegmentIndex == 2{
+                tipPercentage = 18
+            }
+            
+            calculateTotal()
+        }
+   
+    }
+    
+    func calculateTotal(){
+        tip = Double(billAmount * (tipPercentage/100))
+        calculatedTipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(tip + billAmount)"
+    }
+    
+    @IBAction func sliderPositionChanged(_ sender: UISlider) {
+        print("\(sender.value.rounded())")
+        customTipPercentageLabel.text = "\(Int(sender.value.rounded()))% "
+        tipPercentage = Double(sender.value.rounded())
+        calculateTotal()
+    }
 }
 
